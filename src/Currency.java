17a17ae224ada083c0c3eb65a2b95aa2c3fc44ac
gda3 <gerarddonairealos@gmail.com>
2018-01-03 12:50:49 +0100

    Quart Commit

---
+++ src/Currency.java
@@ -0,0 +1,11 @@
+package cat.udl.eps.ep.data;
+
+public class Currency {
+        public Currency(String name){}
+
+        public boolean equals(Object other){}
+
+        public int hashCode(){}
+
+        public String toString(){}
+}
